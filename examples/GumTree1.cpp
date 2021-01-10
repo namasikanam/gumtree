58ec13403484f54bdeb69d2d56321fd6a6ed0d47
TA <namasikanam@gmail.com>
2021-01-10 21:24:56 +0800

    add cpp examples

---
+++ examples/GumTree1.cpp
@@ -0,0 +1,8 @@
+#include <string>
+using namespace std;
+
+string foo(int i) {
+    if (i == 0) return "Foo!";
+}
+
+int main() {}
---
+++ examples/GumTree2.cpp
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+
+string foo(int i) {
+    if (i == 0) return "Bar";
+    else if (i == -1) return "Foo!";
+}
+
+int main() {}
